3d745fb7bb9f2a7e55a12389b088a401e2e1ac79
Itthiphon <itthiphonseeso@gmail.com>
2026-05-07 20:41:12 +0700

    fix url th

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en', 'th'],
-    defaultLocale: 'en',
-  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'source.unsplash.com' },
